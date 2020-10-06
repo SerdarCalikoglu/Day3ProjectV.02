02fe9782a0af36d05239fe58bb2fca11359f3523
SerdarCalikoglu <serdarcalikoglu54@gmail.com>
2020-10-06 20:46:29 +0100

    ?anew commit with chngng

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,8 @@
+public class AwesomeTest {
+
+    // red means it is untracked
+    // green means it is mean undr staging area
+    //grey in.idea folder means it is ignored
+    // when the coloir is normlameans that it was commited
+
+}
