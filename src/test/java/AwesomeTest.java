1be8b6fd0107250888d54da42f2aa82a24f154e8
SerdarCalikoglu <serdarcalikoglu54@gmail.com>
2020-10-06 20:59:18 +0100

    add it more awesome chnages

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -5,4 +5,9 @@ public class AwesomeTest {
     //grey in.idea folder means it is ignored
     // when the coloir is normlameans that it was commited
 
+    // i just pull the new change from github that i did not have already
+    // and  now my project is in sync  with remote repository
+
+    //Now I made more awesome changes  and I am about the push this back to Githup
+
 }
